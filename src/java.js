7037f4f23e5528e1a4a8e69af7ca87713576a527
jeremyda173 <jh4017034@gmail.com>
2023-04-06 13:13:29 -0400

    Commit 3

--- src/java.js
+++ src/java.js
@@ -1,5 +1,29 @@
-const refresh = document.getElementById("#refresh")
-refresh.addEventListener("click", (e) =>{ 
-    e.preventDefault()
-    location.reload();
-})
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// Configurar el índice del carrusel
+let currentIndex = 0;
+
+// Agregar eventos click a los botones del carrusel
+document.querySelector('#prevBtn').addEventListener('click', () => {
+  if (currentIndex === 0) {
+    currentIndex = carouselImages.length - 1;
+  } else {
+    currentIndex--;
+  }
+  updateSlidePosition();
+});
+
+document.querySelector('#nextBtn').addEventListener('click', () => {
+  if (currentIndex === carouselImages.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  updateSlidePosition();
+});
+
+// Actualizar la posición del carrusel
+function updateSlidePosition() {
+  carouselSlide.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
